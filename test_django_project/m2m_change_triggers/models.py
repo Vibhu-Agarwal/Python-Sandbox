0e538bffc8beb0951c5912b59f4baa5b9baf1fda
Vibhu Agarwal <vibhu4agarwal@gmail.com>
2020-11-10 18:45:31 +0530

    Added issue in 'm2m_change_triggers'

--- test_django_project/m2m_change_triggers/models.py
+++ test_django_project/m2m_change_triggers/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+from django.db.models.signals import m2m_changed
+from django.dispatch import receiver
+
+User = get_user_model()
+
+
+class ProjectRick(models.Model):
+    name = models.CharField(max_length=20)
+    admin_users = models.ManyToManyField(User, related_name='admin_rick_projects', blank=True)
+    users = models.ManyToManyField(User, related_name='rick_projects', blank=True)
+
+    def save(self, force_insert=False, force_update=False, using=None,
+             update_fields=None):
+        super(ProjectRick, self).save(force_insert=False, force_update=False, using=None,
+                                      update_fields=None)
+        for admin in self.admin_users.all():
+            if not self.users.filter(pk=admin.id).exists():
+                self.users.add(admin)
+
+
+class ProjectMorty(models.Model):
+    name = models.CharField(max_length=20)
+    admin_users = models.ManyToManyField(User, related_name='admin_morty_projects', blank=True)
+    users = models.ManyToManyField(User, related_name='morty_projects', blank=True)
+
+
+@receiver(m2m_changed, sender=ProjectMorty.admin_users.through)
+def update_users_on_change(instance, action, **kwargs):
+    if action == 'post_add':
+        instance.name = f"{instance.name}_changed"  # changes to this field are reflected in DB
+        instance.save()
+        users = instance.users.all()
+        for admin in instance.admin_users.all():
+            if not users.filter(pk=admin.id).exists():
+                instance.users.add(admin)
--- test_django_project/m2m_change_triggers/tests.py
+++ test_django_project/m2m_change_triggers/tests.py
@@ -1,3 +1,49 @@
 from django.test import TestCase
+from .models import ProjectRick, ProjectMorty
+from django.contrib.auth import get_user_model
+# from django.core.serializers import serialize
 
-# Create your tests here.
+User = get_user_model()
+
+
+class TestProject(TestCase):
+
+    def setUp(self) -> None:
+        project_data = {
+            'name': 'Project RnM'
+        }
+        users = [
+            {
+                'username': 'vibhu',
+                'email': 'vibhu@vibhu.in',
+                'first_name': 'Vibhu',
+                'phone_number': '+999999999',
+                'password': 'password'
+            },
+            {
+                'username': 'guido',
+                'email': 'guido@guido.in',
+                'first_name': 'Guido',
+                'phone_number': '+999999999',
+                'password': 'password'
+            }
+        ]
+        self.users = (
+            User.objects.create(**users[0]),
+            User.objects.create(**users[1]),
+        )
+        self.project_rick = ProjectRick.objects.create(**project_data)
+        self.project_rick.users.add(self.users[0])
+        # print('SET-UP ProjectRick:', serialize('json', ProjectRick.objects.all()))
+
+        self.project_morty = ProjectMorty.objects.create(**project_data)
+        self.project_morty.users.add(self.users[0])
+        # print('SET-UP ProjectMorty:', serialize('json', ProjectMorty.objects.all()))
+
+    def test_save_fn_on_model(self):
+        self.project_rick.admin_users.add(self.users[1])
+        self.assertEqual(self.project_rick.users.filter(id=self.users[1].id).exists(), True)
+
+    def test_m2m_signal_on_model(self):
+        self.project_morty.admin_users.add(self.users[1])
+        self.assertEqual(self.project_morty.users.filter(id=self.users[1].id).exists(), True)
