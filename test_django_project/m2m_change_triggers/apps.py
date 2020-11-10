d7ab953fc7a72fa84f3dac3bdad08de0a416ec76
Vibhu Agarwal <vibhu4agarwal@gmail.com>
2020-11-10 10:29:50 +0530

    Added a Test Django Project

---
+++ test_django_project/m2m_change_triggers/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class M2MChangeTriggersConfig(AppConfig):
+    name = 'm2m_change_triggers'
---
+++ test_django_project/related_name_in_child_w_inheritance/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RelatedNameInChildWInheritanceConfig(AppConfig):
+    name = 'related_name_in_child_w_inheritance'
